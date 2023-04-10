9e3c9124efccd44cc0c656fc2a268b1ec1a4d389
Julian Allende Russek <jallende@bu.edu>
2023-04-10 04:56:15 -0400

    saving changes

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require("express");
-const app = express();
 
 const { exec } = require("child_process");
 const { spawn } = require("child_process");
-const { log } = require("console");
-const readXlsxFile = require('read-excel-file/node')
-const fs = require("fs")
+const bodyParser = require('body-parser');
+const readXlsxFile = require("read-excel-file/node");
+const fs = require("fs");
+const ExcelJS = require('exceljs');
 
-// cors -> 
+const app = express();
+
+
+let wb = new ExcelJS.Workbook();
+
+
+// Middleware
+app.use(bodyParser.json());
+// cors ->
 
 app.get("/", function (req, res) {
   return res
@@ -15,87 +23,110 @@ app.get("/", function (req, res) {
     .json({ success: true, message: "Integration Server Running" });
 });
 
-app.get("/purchase_order/create", async function (req, res) {
-  await readXlsxFile("./templates/purchase_order/ordr.xlsx").then((data) => {
-    
+app.post("/test", function (req, res) {
+  const { body } = req; 
+
+  const fileName = "./templates/purchase_order/ordr.xlsx"
+  
+  wb.xlsx
+    .readFile(fileName)
+    .then( async () => {
+      const ws = await wb.getWorksheet("Sheet1");
+      
+      let firstRow = ws.getRow(1).values;
+      let secondRow = ws.getRow(2).values;
+      let thirdRow = ws.getRow(3);
+
+      firstRow.shift()
+      secondRow.shift()
+
+      const entries = Object.entries(body)
+      const keys = Object.keys(body)
+
+      let result = [[], [], []];
+
+      // Maybe here rearrange the values from the backend 
+      
+      // Get body values and using mapping - Filter them 
+
+      for (let i = 0; i < firstRow.length; i++) {
+        const value = firstRow[i];
+        if (keys.includes(value)) {
+          for (let j = 0; j < entries.length; j++) {
+            if (entries[j][0] === value) {
+              result[0].push(value);
+              result[1].push(secondRow[i]);
+              if (value === "DocNum") {
+                result[2].push("1");
+                continue
+              }
+              result[2].push(entries[j][1]);
+            }
+          }
+        }
+      }
+
+      createTxtFile(result[0], result[1], result[2]);
+
+      return res.status(200).json({ success: true, message: "Integration Server Running" });
+    })
+    .catch((err) => {
+      console.log(err.message);
+      return res
+      .status(400)
+      .json({ success: false, message: "Error on integration" });
+    });
+});
+
+app.get("/purchase_order", async function (req, res) {
+  await readXlsxFile("./templates/purchase_order/ordr.xlsx").then( async (data) => {
     const filtered = {
       columns: [],
       columnsTwo: [],
-      values: []
-    }
+      values: [],
+    };
 
-    for (let i =0; i < data[0].length; i++) {
-      const value = data[2][i]
+    for (let i = 0; i < data[0].length; i++) {
+      const value = data[2][i];
       if (value) {
         filtered.columns.push(data[0][i]);
         filtered.columnsTwo.push(data[1][i]);
-        filtered.values.push(value)
+        filtered.values.push(value);
       }
     }
 
-    
-    createTxtFile(filtered.columns, filtered.columnsTwo, filtered.values)
+    await createTxtFile(filtered.columns, filtered.columnsTwo, filtered.values);
 
-    return res
-    .status(200)
-    .json({ success: true, data: data });
-  })
+    return res.status(200).json({ success: true, data: data });
+  });
 });
 
-// function createTabDelimitedFile(filtered) {
-//   // Combine the arrays into a 2D array for easier processing
-  
-//   // Create a string with the header row
-//   let fileContent = '';
 
-//   // Loop through the arrays and append each row to the file content
-
-//   const values = Object.values(filtered);
-//   for (let i =0; i < values.length ;i++) {
-//     const valuesArr = values[i];
-//     for (let j =0; j < valuesArr.length; j++) {
-//       const value = valuesArr[j];
-//       fileContent += `${value}\t`
-//     }
-//     fileContent += `\n`
-//   }
-//   console.log(fileContent)
-//   // Write the file to disk
-//   fs.writeFileSync('output.txt', fileContent);
-// }
-
-
-function createTxtFile(arr1, arr2, arr3) {
-  let text = '';
+async function createTxtFile(arr1, arr2, arr3) {
+  let text = "";
 
   const transverseArr = (arr) => {
     for (let i = 0; i < arr.length; i++) {
-      if(i === arr.length - 1) {
-        console.log(" FINAL COUNTDOWN");
-        text += arr[i] + '\n'
-        break
+      if (i === arr.length - 1) {
+        text += arr[i] + "\n";
+        break;
       }
-      text += arr[i] + '\t'
+      text += arr[i] + "\t";
       // text += arr1[i] + '\t' + arr2[i] + '\t' + arr3[i] + '\n';
     }
-    console.log(text)
-  }
+    console.log(text);
+  };
 
-  transverseArr(arr1)
-  transverseArr(arr2)
-  transverseArr(arr3)
+  transverseArr(arr1);
+  transverseArr(arr2);
+  transverseArr(arr3);
 
-  fs.writeFile(
-    "output.txt",
-    text,
-    (err) => {
-      if (err) {
-        console.error(err);
-        return 
-      }
-     }
-  )
-
+  await fs.writeFile("output.txt", text, (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+  });
 }
 
 app.post("/purchase_order", function (req, res) {
@@ -109,12 +140,12 @@ app.post("/purchase_order", function (req, res) {
     (err) => {
       if (err) {
         console.error(err);
-        return 
+        return;
       }
       console.log("Running script");
-      runScript("./executables/hello.sh")
+      runScript("./executables/hello.sh");
     }
-  )
+  );
 
   return res
     .status(200)
@@ -131,7 +162,7 @@ app.listen(4000, () => {
   console.log("Listening on port 4000");
 });
 
-function runScript (scriptPath) {
+function runScript(scriptPath) {
   // Replace 'path/to/script.sh' with the path to your bash script
 
   // Spawn a new process to execute the script
