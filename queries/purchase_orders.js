68e62be2cfe7bf64cd617366070d2d85b862b70a
Julian Allende Russek <jallende@bu.edu>
2023-05-09 22:38:28 -0400

    saving changes

--- queries/purchase_orders.js
+++ queries/purchase_orders.js
@@ -1,12 +1,15 @@
 const sql = require('mssql')
 
-async () => {
+const test = async() => {
+    console.log("Running this")
     try {
         // make sure that any items are correctly URL encoded in the connection string
-        await sql.connect('Server=localhost,1433;Database=AMD_Entrenamiento;User Id=U0002;Password=S@p123;Encrypt=true')
+        await sql.connect('Server=localhost,1433;Database=AMD_Entrenamiento;User Id=U002;Password=S@p123;Encrypt=true')
         const result = await sql.query`select * from ORDR`
         console.dir(result)
     } catch (err) {
         // ... error checks
     }
 }
+
+console.log(test())
