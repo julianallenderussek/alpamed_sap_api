0aab60fc66026b6d5f16d01addcdf14081168fda
Julian Allende <julianallenderussek@gmail.com>
2023-06-20 22:36:19 -0600

    taking out pause

--- executables/purchase_order/update/Update.bat
+++ executables/purchase_order/update/Update.bat
@@ -3,5 +3,4 @@
 IF EXIST "C:\Users\U002\Desktop\alpamed_sap_api\files\purchase_order\update\ordr.txt" ("C:\Program Files\SAP\Data Transfer Workbench\dtw.exe" -s C:\Users\U002\Desktop\alpamed_sap_api\executables\purchase_order\update\update.xml 
 echo Se cargo ORDR Update) ELSE (echo ORDR Update no existe.)
 
-pause
-exit
+exit|
